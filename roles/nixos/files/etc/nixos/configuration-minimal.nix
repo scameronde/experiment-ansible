b60db2c68fdcd1afc39b114ae893d707606786cd
Steffen Eichenberg <Steffen.Eichenberg@lindenbaum.eu>
2019-09-09 11:08:32 +0200

    Add NUR to all configurations

--- roles/nixos/files/etc/nixos/configuration-minimal.nix
+++ roles/nixos/files/etc/nixos/configuration-minimal.nix
@@ -20,6 +20,11 @@
   nixpkgs = {
     config = {
       allowUnfree = true;
+      packageOverrides = pkgs: {
+        nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+          inherit pkgs;
+        };
+      };
     };
   };
 
--- roles/nixos/files/etc/nixos/configuration.nix
+++ roles/nixos/files/etc/nixos/configuration.nix
@@ -26,6 +26,9 @@ in
         unstable = import unstableTarball {
           config = config.nixpkgs.config;
         };
+        nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+          inherit pkgs;
+        };
       };
       allowUnfree = true;
     };
