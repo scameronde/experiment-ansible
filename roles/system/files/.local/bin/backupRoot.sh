1d4ce4b89adc6c952fe5670da3602c48c74aabbd
Steffen Eichenberg <Steffen.Eichenberg@lindenbaum.eu>
2019-11-20 10:22:32 +0100

    Change backup scripts

---
+++ roles/system/files/.local/bin/backupRoot.sh
@@ -0,0 +1,2 @@
+sudo rsync -aAX --info=progress2 --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/home/*","/lost+found","/nix/*","/var/lib/libvirt/images/*","/var/lib/docker"} / /run/media/eichens/Backup/root
+
